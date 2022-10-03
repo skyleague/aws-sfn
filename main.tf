f82fe1aa20818d86625abe084917a1a026dd73de
MickVanDuijn <m.vanduijn@zefiros.io>
2022-10-03 15:32:27 +0200

    feat: use therefore-sfn cli (#2)

--- main.tf
+++ main.tf
@@ -1,18 +1,26 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "external" "definition" {
-  program = ["npx", "-y", "ts-node", "-T", "--skip-project", "${path.module}/scripts/src/index.ts"]
+  count = endswith(var.definition.file, ".json") ? 0 : 1
+  program = flatten([
+    "npx", "-y",
+    # Install dependencies for npx
+    [for p in setunion(["@skyleague/therefore-sfn@^1.3.6"], var.npm_package_dependencies) : ["--package", p]],
 
-  query = {
-    file         = var.definition.file
-    export       = var.definition.export
-    awsRegion    = data.aws_region.current.name
-    awsAccountId = data.aws_caller_identity.current.account_id
-  }
+    # Execute therefore-sfn
+    "therefore-sfn", "compile", var.definition.file,
+    "--export", var.definition.export,
+    "--aws-region", data.aws_region.current.name,
+    "--aws-account-id", data.aws_caller_identity.current.account_id
+  ])
 }
 locals {
-  lambda_arns = jsondecode(data.external.definition.result.lambda_arns)
-  definition  = jsondecode(data.external.definition.result.definition)
+  raw_definition = endswith(var.definition.file, ".json") ? jsondecode(templatefile(var.definition.file, {
+    aws_region     = data.aws_region.current.name
+    aws_account_id = data.aws_caller_identity.current.account_id
+  })) : data.external.definition[0].result
+  lambda_arns = jsondecode(local.raw_definition.lambda_arns)
+  definition  = jsondecode(local.raw_definition.definition)
 }
 
 resource "aws_sfn_state_machine" "this" {
