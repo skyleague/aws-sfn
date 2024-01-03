ee8eeeac3b86f50bc83065a76c8ba573b1c291ea
MickVanDuijn <m.vanduijn@zefiros.io>
2024-01-03 16:57:13 +0100

    feat(node): upgrade minimum version (#11)
    BREAKING CHANGE: upgrade minimum version to 20

---
+++ scripts/vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        setupFiles: ['./test/__test__/setup.ts'],
+        include: ['./src/**/*.{test,spec}.{ts,js}', './test/**/*.{test,spec}.{ts,js}'],
+        coverage: {
+            reportsDirectory: './.coverage',
+        },
+        fakeTimers: {
+            now: new Date(2022, 1, 10),
+            toFake: [
+                // 'setTimeout',
+                // 'setImmediate',
+                'clearTimeout',
+                'setInterval',
+                'clearInterval',
+                'clearImmediate',
+                'Date',
+            ],
+        },
+    },
+})
